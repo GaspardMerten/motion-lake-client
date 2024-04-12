8b05388706ed96405a533c6e3e7ffe61bae9cb7b
Gaspard Merten <gaspard@norse.be>
2024-04-12 19:45:55 +0200

    Cleaning up, adding black format

--- src/motion_lake_client/client.py
+++ src/motion_lake_client/client.py
@@ -1,10 +1,25 @@
 import dataclasses
 from datetime import datetime
+from enum import Enum
 from typing import Protocol, Dict, Any, List
 
 import requests
 
-__all__ = ['Item', 'Collection', 'NetworkClient', 'RequestsClient', 'InnerClient', 'BaseClient']
+__all__ = [
+    "Item",
+    "Collection",
+    "NetworkClient",
+    "RequestsClient",
+    "InnerClient",
+    "BaseClient",
+    "ContentType",
+    "MAX_DATE",
+    "MIN_DATE",
+]
+
+# Hopefully, we have better storage systems in 2250...
+MAX_DATE = datetime(2250, 12, 31, 23, 59, 59, 999999)
+MIN_DATE = datetime(1971, 1, 1, 0, 0, 0, 0)
 
 
 @dataclasses.dataclass
@@ -12,6 +27,7 @@ class Item:
     """
     A data item in the collection.
     """
+
     timestamp: datetime
     data: bytes
 
@@ -21,10 +37,19 @@ class Collection:
     """
     A summary of a collection. A collection is a group of items.
     """
+
     name: str
     min_timestamp: datetime
     max_timestamp: datetime
     count: int
+
+
+class ContentType(Enum):
+    JSON = 0
+    RAW = 1
+    GTFS_RT = 2
+    CSV = 3
+    GTFS = 4
 
 
 class NetworkClient(Protocol):
@@ -32,11 +57,9 @@ class NetworkClient(Protocol):
     A network client to make requests to the storage server.
     """
 
-    def get(self, url: str, query_params: Dict[str, Any] = None) -> dict:
-        ...
+    def get(self, url: str, query_params: Dict[str, Any] = None) -> dict: ...
 
-    def post(self, url: str, body: dict) -> dict:
-        ...
+    def post(self, url: str, body: dict) -> dict: ...
 
 
 class RequestsClient(NetworkClient):
@@ -49,17 +72,33 @@ class RequestsClient(NetworkClient):
         self.base_url = base_url
 
     def get(self, url: str, query_params: Dict[str, Any] = None) -> dict:
-        response = requests.get(self.base_url + url, params=query_params)
-        return response.json()
+        try:
+            return requests.get(self.base_url + url, params=query_params).json()
+        except requests.exceptions.RequestException as e:
+            return {"error": "A request error occurred."}
+        except requests.exceptions.JSONDecodeError as e:
+            return {"error": "The response could not be decoded."}
 
     def post(self, url: str, body: dict) -> dict:
-        response = requests.post(self.base_url + url, json=body)
-        return response.json()
+        try:
+            return requests.post(self.base_url + url, json=body).json()
+        except requests.exceptions.RequestException as e:
+            return {"error": "A request error occurred."}
+        except requests.exceptions.JSONDecodeError as e:
+            return {"error": "The response could not be decoded."}
 
 
 class InnerClient:
     def __init__(self, network_client: NetworkClient):
         self.network_client = network_client
+
+    def flush_buffer(self, collection_name: str) -> dict:
+        """
+        Flush the buffered data in the collection with the given name.
+        :param collection_name: The name of the collection to flush
+        :return: None
+        """
+        return self.network_client.post(f"/flush/{collection_name}/", {})
 
     def create_collection(self, collection_name: str) -> dict:
         """
@@ -69,8 +108,14 @@ class InnerClient:
         """
         return self.network_client.post(f"/collection/", {"name": collection_name})
 
-    def store(self, collection_name: str, data: bytes, timestamp: int, content_type: str = None,
-              create_collection: bool = False) -> dict:
+    def store(
+        self,
+        collection_name: str,
+        data: bytes,
+        timestamp: int,
+        content_type: ContentType = None,
+        create_collection: bool = False,
+    ) -> dict:
         """
         Store the given data in the collection with the given name.
         :param collection_name: The name of the collection to store the data in
@@ -82,12 +127,23 @@ class InnerClient:
         """
         return self.network_client.post(
             f"/store/{collection_name}/",
-            {"data": data.hex(), "timestamp": timestamp, "content_type": content_type,
-             "create_collection": create_collection, }
+            {
+                "data": data.hex(),
+                "timestamp": timestamp,
+                "content_type": content_type.value if content_type else None,
+                "create_collection": create_collection,
+            },
         )
 
-    def query(self, collection_name: str, min_timestamp: int, max_timestamp: int, ascending: bool,
-              limit: int = None, offset: int = None) -> dict:
+    def query(
+        self,
+        collection_name: str,
+        min_timestamp: int,
+        max_timestamp: int,
+        ascending: bool,
+        limit: int = None,
+        offset: int = None,
+    ) -> dict:
         """
         Query the data in the collection with the given name.
         :param collection_name: The name of the collection to query
@@ -104,9 +160,9 @@ class InnerClient:
                 "min_timestamp": min_timestamp,
                 "max_timestamp": max_timestamp,
                 "ascending": ascending,
-                "limit": limit,
-                "offset": offset
-            }
+                "limit": limit or 1,
+                "offset": offset or 0,
+            },
         )
 
     def get_collections(self) -> List[Collection]:
@@ -118,18 +174,45 @@ class InnerClient:
         return [
             Collection(
                 name=collection["name"],
-                min_timestamp=datetime.fromisoformat(collection["min_timestamp"]) if collection[
-                    "min_timestamp"] else None,
-                max_timestamp=datetime.fromisoformat(collection["max_timestamp"]) if collection[
-                    "max_timestamp"] else None,
-                count=collection["count"]
+                min_timestamp=(
+                    datetime.fromisoformat(collection["min_timestamp"])
+                    if collection["min_timestamp"]
+                    else None
+                ),
+                max_timestamp=(
+                    datetime.fromisoformat(collection["max_timestamp"])
+                    if collection["max_timestamp"]
+                    else None
+                ),
+                count=collection["count"],
             )
             for collection in collections
         ]
 
+    def advanced_query(
+        self,
+        collection_name: str,
+        query: str,
+        min_timestamp: datetime,
+        max_timestamp: datetime,
+    ):
+        assert (
+            "[table]" in query
+        ), """Query must contain [table] placeholder (it will be replaced with a view on a 
+        table)"""
+
+        return self.network_client.post(
+            f"/advanced/{collection_name}/",
+            {
+                "query": query,
+                "min_timestamp": int(min_timestamp.timestamp() * 1000),
+                "max_timestamp": int(max_timestamp.timestamp() * 1000),
+            },
+        )
+
 
 class BaseClient:
-    def __init__(self, lake_url: str = 'http://localhost:8000'):
+    def __init__(self, lake_url: str = "http://localhost:8000"):
         """
         Initialize the client with the base URL of the storage server.
         :param lake_url: The base URL of the storage server
@@ -138,52 +221,110 @@ class BaseClient:
 
     @staticmethod
     def _parse_server_timestamp(timestamp: int) -> datetime:
-        return datetime.fromtimestamp(timestamp / 1000)
+        return datetime.fromtimestamp(timestamp)
 
     def _parse_results(self, results: List[dict]) -> List[Item]:
         return [
             Item(
                 timestamp=self._parse_server_timestamp(item["timestamp"]),
-                data=bytes.fromhex(item["data"])
+                data=bytes.fromhex(item["data"]),
             )
             for item in results
         ]
 
+    def flush_buffer(self, collection_name: str) -> dict:
+        return self.inner_client.flush_buffer(collection_name)
+
     def create_collection(self, collection_name: str) -> dict:
         return self.inner_client.create_collection(collection_name)
 
-    def store(self, collection_name: str, data: bytes, timestamp: int, content_type: str = None,
-              create_collection: bool = False) -> dict:
-        return self.inner_client.store(collection_name, data, timestamp, content_type, create_collection)
+    def store(
+        self,
+        collection_name: str,
+        data: bytes,
+        timestamp: int,
+        content_type: ContentType = None,
+        create_collection: bool = False,
+    ) -> dict:
+        return self.inner_client.store(
+            collection_name, data, timestamp, content_type, create_collection
+        )
 
-    def query(self, collection_name: str, min_timestamp: int, max_timestamp: int, ascending: bool,
-              limit: int = None, offset: int = 0) -> dict:
-        return self.inner_client.query(collection_name, min_timestamp, max_timestamp, ascending, limit, offset)
+    def query(
+        self,
+        collection_name: str,
+        min_datetime: datetime,
+        max_datetime: datetime,
+        ascending: bool,
+        limit: int = None,
+        offset: int = 0,
+    ) -> dict:
+        min_datetime = min_datetime or MIN_DATE
+        max_datetime = max_datetime or MAX_DATE
+
+        return self.inner_client.query(
+            collection_name,
+            int(min_datetime.timestamp() * 1000),
+            int(max_datetime.timestamp() * 1000),
+            ascending,
+            limit,
+            offset,
+        )
 
     def get_last_items(self, collection_name: str, limit: int) -> List[Item]:
-        response = self.query(collection_name, 0, int(datetime.now().timestamp()), False, limit)
-        return self._parse_results(response['results'])
+        response = self.query(collection_name, MIN_DATE, datetime.now(), False, limit)
+        return self._parse_results(response["results"])
 
     def get_last_item(self, collection_name: str) -> Item:
-        return self.get_last_items(collection_name, 1)[0]
+        results = self.get_last_items(collection_name, 1)
+        return (results or [None])[0]
 
     def get_first_items(self, collection_name: str, limit: int) -> List[Item]:
-        response = self.query(collection_name, 0, int(datetime.now().timestamp()), True, limit)
-        return self._parse_results(response['results'])
+        response = self.query(collection_name, MIN_DATE, datetime.now(), True, limit)
+        return self._parse_results(response["results"])
 
     def get_first_item(self, collection_name: str) -> Item:
-        return self.get_first_items(collection_name, 1)[0]
+        items = self.get_first_items(collection_name, 1)
 
-    def get_items_between(self, collection_name: str, min_timestamp: int, max_timestamp: int, ascending: bool = True,
-                          limit: int = None, offset: int = None) -> List[Item]:
-        response = self.query(collection_name, min_timestamp, max_timestamp, ascending, limit, offset)
-        return self._parse_results(response['results'])
+        return (items or [None])[0]
 
-    def get_items_before(self, collection_name: str, timestamp: int, limit: int) -> List[Item]:
-        return self.get_items_between(collection_name, 0, timestamp, False, limit)
+    def get_items_between(
+        self,
+        collection_name: str,
+        min_datetime: datetime,
+        max_datetime: datetime,
+        ascending: bool = True,
+        limit: int = None,
+        offset: int = None,
+    ) -> List[Item]:
+        response = self.query(
+            collection_name, min_datetime, max_datetime, ascending, limit, offset
+        )
+        return self._parse_results(response["results"])
 
-    def get_items_after(self, collection_name: str, timestamp: int, limit: int) -> List[Item]:
-        return self.get_items_between(collection_name, timestamp, int(datetime.now().timestamp()), True, limit)
+    def get_items_before(
+        self, collection_name: str, date: datetime, limit: int
+    ) -> List[Item]:
+        return self.get_items_between(collection_name, MIN_DATE, date, False, limit)
+
+    def get_items_after(
+        self, collection_name: str, timestamp: datetime, limit: int
+    ) -> List[Item]:
+        return self.get_items_between(collection_name, timestamp, MAX_DATE, True, limit)
 
     def get_collections(self) -> List[Collection]:
         return self.inner_client.get_collections()
+
+    def advanced_query(
+        self,
+        collection_name: str,
+        query: str,
+        min_timestamp: datetime,
+        max_timestamp: datetime,
+    ):
+        assert (
+            "[table]" in query
+        ), """Query must contain [table] placeholder (it will be replaced with a view on a table)"""
+        return self.inner_client.advanced_query(
+            collection_name, query, min_timestamp, max_timestamp
+        )
